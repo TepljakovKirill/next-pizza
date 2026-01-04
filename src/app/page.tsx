9c21839178ab364402ba8ea7cde7148849baed56
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2026-01-04 20:49:07 +0300

    install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Home</h1>
+      <Button variant="outline">Корзина</Button>
+    </div>
   );
 }
